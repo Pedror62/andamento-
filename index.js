2c3173047126900f19e0e890dc49b6217ee6a688
pedror62 <102195011+Pedror62@users.noreply.github.com>
2022-10-05 21:19:13 -0300

    enviar interativo
    commit button interativo

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-//alert('arlert')
+/*var function onclik(){
+
+    
+    let tel = window.document.querySelector('inpult#tel') 
+    let res =  window.document.querySelector('input#res')
+    let vel = Text(tel.enviar)
+    res.innerHTML= {enviar}*/
+
+
+    function myFunction() {
+        alert("enviado!")
+            
+    }
