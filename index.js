85db39a7bf86ae4cd10151cbe2718d314a5e4260
pedror62 <102195011+Pedror62@users.noreply.github.com>
2023-01-11 21:19:03 -0300

    nova atualização
    aplicação de Emojis e preparação para novas funcionalidades.

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-/*var function onclik(){
+/*var function onclik() {
 
-    
+    '&#128233'
     let tel = window.document.querySelector('inpult#tel') 
     let res =  window.document.querySelector('input#res')
     let vel = Text(tel.enviar)
-    res.innerHTML= {enviar}*/
+    res.innerHTML= {enviar}}*/
 
 
     function myFunction() {
-        alert("enviado!")
+        alert("enviado!"
+        )  
             
     }
